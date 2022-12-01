f7983176f6838e791675cfc7c3224ec50745ca05
⛧﻿JΣƧ ░ FӨƧƬΣЯ⛧ <113276165+Jes-Foster@users.noreply.github.com>
2022-11-30 20:23:24 -0600

    Abre el archivo
    abre el archivo pero no hace nada

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -9,6 +9,6 @@ void main(void)
 	FILE* fp; //FILE POINTER (PUNTERO DE ARCHIVO)
 	printf("COLOQUE LA RUTA O NOMBRE DEL ARCHIVO: ");
 	scanf_s("%s", ARCHIVO, 255);
-	printf("el nombre del archivo es: %s", ARCHIVO);
+	fopen_s(&fp, ARCHIVO, "r"); //r es de lectura (read) // el nombre almacenado se lo da a fp para indicar que es un archivo 
 }
 
